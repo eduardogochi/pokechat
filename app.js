562d8e396bbd207be8c78d21702e885cd6e5e0e8
Eduardo Gochi <egochi@tiempodevelopment.com>
2015-10-25 15:30:43 -0600

    Creating components

---
+++ app.js
@@ -0,0 +1,17 @@
+/*
+	Module Dependencies
+*/
+
+import React from 'react';
+import PokeTable from './components/PokeTable';
+
+		var pokemons = [
+			{number: 1, name: 'Bulbasaur'},
+			{number: 2, name: 'Ivysaur'},
+			{number: 3, name: 'Venasaur'},
+		];
+
+		React.render(
+			<PokeTable pokemons={pokemons} />,
+			document.getElementById('container')
+		);
