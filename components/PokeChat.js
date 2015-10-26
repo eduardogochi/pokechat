a593be6afd3ad4bce253df513570220f2c6366f8
Eduardo Gochi <egochi@tiempodevelopment.com>
2015-10-25 22:46:53 -0600

    Added animation to messages with react-addon CSSTransitionGroup

--- components/PokeChat.js
+++ components/PokeChat.js
@@ -2,17 +2,21 @@
 	Module Dependencies
 */
 
-import React from 'react';
+import React from 'react/addons';
 import PokeMessage from './PokeMessage';
+
+const {CSSTransitionGroup} = React.addons;
 
 export default class PokeChat extends React.Component{
 	render(){
 		return <ul className="pokechat">
+		<CSSTransitionGroup transitionName="message-animation">
 			{
 				this.props.messages.map((message) => {
 					return <PokeMessage key={message.id} message={message} />
 				})
 			}
+		</CSSTransitionGroup>
 		</ul>
 	}
 }
